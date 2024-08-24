d1ee6c309a9b9c9e89418a954b18c08faac24836
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-08-24 11:03:00 +0530

    Time: 10 ms (84.42%), Space: 20.8 MB (34.6%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i=0,j=nums.size();
-        while(i<j)
+        //first method->tc=O(nlogn),sc=O(1)
+        // sort(nums.begin(),nums.end());
+        // int i=0,j=nums.size();
+        // while(i<j)
+        // {
+        //     if(nums[i]!=i)
+        //     {
+        //         return i;
+        //     }
+        //     i++;
+        // }
+        // return j;
+        //secnod method tc=O(n),sc=O(1);
+        int sum=0,n=nums.size();
+        sum=(n*(n+1))/2;
+        for(int i=0;i<n;i++)
         {
-            if(nums[i]!=i)
-            {
-                return i;
-            }
-            i++;
+            sum=sum-nums[i];
         }
-        return j;
+        return sum;
+
         
 
         
