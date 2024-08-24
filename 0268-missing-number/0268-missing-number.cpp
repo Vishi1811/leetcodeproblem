b53ce89ff42ad45629dd6a2b6849b6d29a4ac18e
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-08-24 11:45:39 +0530

    Time: 13 ms (59.64%), Space: 20.8 MB (34.6%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -15,18 +15,18 @@ public:
         // return j;
 
 
-        // //secnod method tc=O(n),sc=O(1);
-        // int sum=0,n=nums.size();
-        // sum=(n*(n+1))/2;
-        // for(int i=0;i<n;i++)
-        // {
-        //     sum=sum-nums[i];
-        // }
-        // return sum;
+        //secnod method tc=O(n),sc=O(1);
+        int sum=0,n=nums.size();
+        sum=(n*(n+1))/2;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum-nums[i];
+        }
+        return sum;
 
 
 
-        /*third method ,this is slighly better of second because in the second method if n>10^5 then in computation of n*n+1 lead to 10^10 that can not store by int data type having said that tc and sc are same as second*/
+        /*third method ,this is slighly better of second because in the second method if n>10^5 then in computation of n*n+1 lead to 10^10 that can not store by int data type having said that tc and sc are same as second*  but for this question second is best because 0<n<=10^4
         int xor1=0,xor2=0;
         for(int i=0;i<nums.size();i++)
         {
@@ -34,5 +34,6 @@ public:
             xor2=xor2^nums[i];
         }
         return (xor1^xor2);
+    }*/
     }
 };
