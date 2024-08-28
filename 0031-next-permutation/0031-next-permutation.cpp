bf18704643340a7827edea2d639605f862d8918d
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-08-28 13:30:35 +0530

    Time: 7 ms (15.5%), Space: 14.9 MB (43.28%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int n=nums.size();
+        int index=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                index=i;
+                break;
+            }
+        }
+        if(index==-1) {
+            reverse(nums.begin(),nums.end());
+            return;}
+        for(int i=n-1;i>=index;i--)
+        {
+            if(nums[i]>nums[index])
+            {
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin()+(index+1),nums.end());
     }
 };
