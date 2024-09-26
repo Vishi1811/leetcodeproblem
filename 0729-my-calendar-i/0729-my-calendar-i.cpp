27b036dddeb57995c407a6bd8f1800d4d35c71fa
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-09-26 07:06:34 +0530

    Time: 79 ms (61.04%), Space: 41.6 MB (97.04%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,18 @@
+class MyCalendar {
+    private:
+        vector<pair<int, int>> m;
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        for (auto it : m) {
+            if(it.first<end && start<it.second){
+                return false;
+            }
+        }
+        m.push_back({start, end});
+        return true;
+    }
+};
