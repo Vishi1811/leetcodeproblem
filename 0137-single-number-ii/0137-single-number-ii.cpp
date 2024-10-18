44132b0a1bbf14f63cb2dd572978255ef13738d3
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-10-18 11:32:15 +0530

    Time: 0 ms (100%), Space: 12.1 MB (86.34%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -16,13 +16,22 @@ public:
 
         //Approch:2
         
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i+=3)
-        {
-            if(nums[i]!=nums[i-1])return nums[i-1];
-        }
-        return nums[n-1];
+        // int n=nums.size();
+        // sort(nums.begin(),nums.end());
+        // for(int i=1;i<n;i+=3)
+        // {
+        //     if(nums[i]!=nums[i-1])return nums[i-1];
+        // }
+        // return nums[n-1];
 
+        //Approch:3
+
+        int ones=0,twices=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ones=(ones^nums[i])&(~twices);
+            twices=(twices^nums[i])&(~ones);
+        }
+        return ones;
     }
 };
