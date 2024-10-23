a302e873c0da6c804474a58589efff2b1a733fd4
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-10-23 07:37:36 +0530

    Time: 300 ms (90.87%), Space: 515.3 MB (5.57%) - LeetHub

---
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        TreeNode *dummy = root;
+        root->val=0;
+        q.push(dummy);
+        while(q.size()){
+            queue<TreeNode*> temp;
+            map<TreeNode *, int> m;
+            while(q.size()){
+                if(q.front()->left!=nullptr){
+                    temp.push(q.front()->left);
+                    m[q.front()] += q.front()->left->val;
+                }
+                if(q.front()->right!=nullptr){
+                    temp.push(q.front()->right);
+                    m[q.front()] += q.front()->right->val;
+                }
+                q.pop();
+            }
+            int sum=0;
+            for(auto x:m){
+                sum+=x.second;
+            }
+            for(auto x:m){
+                int temp2=0;
+                if(x.first->left!=nullptr){
+                    temp2+=x.first->left->val;
+                }
+                if (x.first->right != nullptr)
+                {
+                    temp2 += x.first->right->val;
+                }
+                temp2=sum-temp2;
+                if (x.first->left != nullptr)
+                {
+                    x.first->left->val=temp2;
+                }
+                if (x.first->right != nullptr)
+                {
+                    x.first->right->val = temp2;
+                }
+            }
+                
+            q=temp;
+        }
+        
+        return root;
+    }
+};
