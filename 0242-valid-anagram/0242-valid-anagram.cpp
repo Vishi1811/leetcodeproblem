d7a3d3dda786d96162078247b81b6f70bd8b980a
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-11-12 15:01:58 +0530

    Time: 7 ms (37.7%), Space: 9.8 MB (39.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t? true:false;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t? true:false;
+        if (s.size() != t.size()) return false;
+
+        unordered_map<char, int> freq;
+
+        // Increment for `s` and decrement for `t`
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i]]++;
+            freq[t[i]]--;
+        }
+
+        // Check if all counts are zero
+        for (auto [key, value] : freq) {
+            if (value != 0) return false;
+        }
+
+        return true;
         
     }
 };
