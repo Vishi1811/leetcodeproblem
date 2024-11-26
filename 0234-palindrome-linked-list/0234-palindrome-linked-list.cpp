cc8d47b3d45b47e103e607a274c1f163b16a46ca
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-11-26 11:48:27 +0530

    Time: 0 ms (100%), Space: 116.8 MB (91.19%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,9 +10,27 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode * head)
+    {
+        if(head==NULL || head->next==NULL)return head;
+        ListNode* front=head->next;
+        ListNode* back=NULL;
+        ListNode*temp=head;
+
+        while(front)
+        {
+           
+            temp->next=back;
+            back=temp;
+            temp=front;
+            front=front->next;
+        }
+        temp->next=back;
+        return temp;
+    }
     bool isPalindrome(ListNode* head) {
         //------>using stack-------->
-        stack<int>st;
+       /* stack<int>st;
         ListNode *temp=head;
         while(temp)
         {
@@ -27,7 +45,30 @@ public:
             temp=temp->next;
         }
         return true;
-
+        time complexity:O(n),space complexity:O(n)*/
+        
+        //<--------Optimum solution------------->
+        ListNode *temp=head;
+        ListNode *slow=head,*fast=head;
+        while(fast->next && fast->next->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode *newhead=reverse(slow->next);
+        temp=head;
+        while(newhead)
+        {
+            if(temp->val!=newhead->val)
+            {
+                reverse(slow->next);
+                return false;
+            }
+            temp=temp->next;
+            newhead=newhead->next;
+        }
+      reverse(slow->next);
+       return true;
         
     }
 };
