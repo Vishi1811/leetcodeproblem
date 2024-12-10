a8e94ad7be77e2176f66023e611650c659fe504f
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-12-10 16:33:14 +0530

    Time: 55 ms (17.76%), Space: 75.8 MB (6.99%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,25 +10,49 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int>v;
-        ListNode *temp=head;
-        while(temp)
+    ListNode * findMiddle(ListNode * h)
+    {
+        ListNode *slow=h;
+        if(slow==NULL || slow->next==NULL)return h;
+        ListNode* fast = h->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode * t1=list1, *t2=list2;
+        ListNode* newhead = new ListNode(-1);
+        ListNode* current = newhead;
+        while(t1 && t2)
         {
-            v.push_back(temp->val);
-            temp=temp->next;
+            if(t1->val<=t2->val)
+            {
+                current->next=t1;
+                t1=t1->next;
+                current=current->next;
+            }
+            else
+            {
+                current->next=t2;
+                t2=t2->next;
+                current=current->next;
+            }
 
         }
-        sort(v.begin(),v.end());
-        temp=head;
-        int i=0;
-        while(i<v.size() && temp)
-        {
-            temp->val=v[i];
-            temp=temp->next;
-            i++;
-        }
-        return head;
-        
+        if(t1)current->next=t1;
+        else current->next=t2;
+        return newhead->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+
+        ListNode *middle=findMiddle(head);
+        ListNode * leftNode=head,*rightNode=middle->next;
+        middle->next=NULL;
+        leftNode=sortList(leftNode);
+        rightNode=sortList(rightNode);
+        return mergeTwoLists(leftNode,rightNode);
     }
 };
