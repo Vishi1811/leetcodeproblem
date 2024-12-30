442c9c62e971bd2828279ab040085f6a9eddf061
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-12-30 06:53:06 +0530

    Time: 6 ms (49.72%), Space: 28.4 MB (52.26%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -6,18 +6,29 @@ public:
         stack<int>st;
         for(int i=2*n-1;i>=0;i--)
         {
-            while(!st.empty() && st.top()<=nums[i%n])st.pop();
-            if(st.empty()){
-                if(i<n)ans[i]=-1;
-                st.push(nums[i%n]);
-                continue;
-            }
-            else if(st.top()>nums[i%n])
-            {
-                if(i<n)ans[i]=st.top();
-            }
-            st.push(nums[i%n]);
+           while(!st.empty() && st.top()<=nums[i%n])st.pop();
+           if(i<n)
+           {
+                ans[i]=st.empty()?-1:st.top();
+           }
+           st.push(nums[i%n]);
+
         }
         return ans;
+        // for(int i=2*n-1;i>=0;i--)
+        // {
+        //     while(!st.empty() && st.top()<=nums[i%n])st.pop();
+        //     if(st.empty()){
+        //         if(i<n)ans[i]=-1;
+        //         st.push(nums[i%n]);
+        //         continue;
+        //     }
+        //     else if(st.top()>nums[i%n])
+        //     {
+        //         if(i<n)ans[i]=st.top();
+        //     }
+        //     st.push(nums[i%n]);
+        // }
+        // return ans;
     }
 };
