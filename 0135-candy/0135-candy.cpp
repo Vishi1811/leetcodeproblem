314962a75383c7ffd4ad7294af0b4d9b861b7da0
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2025-02-12 10:54:00 +0530

    Time: 0 ms (100%), Space: 22.1 MB (71.93%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,32 +2,71 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        vector<int>left(n,1);
-        vector<int>right(n,1);
+        int sum=1,i=1;
+        while(i<n)
+        {
+            if(ratings[i]==ratings[i-1])
+            {
+                sum+=1;
+                i++;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1])
+            {
+                peak+=1;
+                sum+=peak;
+                i++;
+
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1])
+            {
+                sum+=down;
+                down+=1;
+                i++;
+            }
+            if(down>peak)
+            {
+                sum+=(down-peak);
+            }
+        }
+        return sum;
+
+
+
+
+
+
+
+
+
+
+        // vector<int>left(n,1);
+        // vector<int>right(n,1);
         
-        for(int i=1;i<n;i++)
-        {
-            if(ratings[i]>ratings[i-1])
-            {
-                left[i]=left[i-1]+1;
+        // for(int i=1;i<n;i++)
+        // {
+        //     if(ratings[i]>ratings[i-1])
+        //     {
+        //         left[i]=left[i-1]+1;
                 
-            }
+        //     }
            
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            if(ratings[i]>ratings[i+1])
-            {
-                right[i]=right[i+1]+1;
+        // }
+        // for(int i=n-2;i>=0;i--)
+        // {
+        //     if(ratings[i]>ratings[i+1])
+        //     {
+        //         right[i]=right[i+1]+1;
                 
-            }
+        //     }
           
-        }
-        int candy=0;
-        for(int i=0;i<n;i++)
-        {
-            candy+=max(left[i],right[i]);
-        }
-        return candy;
+        // }
+        // int candy=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     candy+=max(left[i],right[i]);
+        // }
+        // return candy;
     }
 };
