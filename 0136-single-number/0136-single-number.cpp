3878e158e06b6f8218f9d4540aeacd9f1d5238fd
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2025-04-23 14:10:25 +0530

    Time: 0 ms (100%), Space: 20.7 MB (59.13%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //this is most optimum solution for this question take O(n)time complexity and O(1) space 
         int ans=0;
-        for(int i=0;i<nums.size();i++)
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
             ans=ans^nums[i];
         }
         return ans;
-
-        // //second method take O(nlon (n/2)+1)+n/2+1 tc and n/2+1 sc 
-        // map<int, int>m;
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     m[nums[i]]++;
-        // }
-        // for(auto it:m)
-        // {
-        //     if(it.second==1)
-        //         return it.first;
-        // }
-        // return 0;
-        
     }
 };
