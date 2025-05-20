93863ee283735cfd8c4f0bd19337f540293e71db
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2025-05-20 16:08:49 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.61%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,30 +1,40 @@
 class Solution {
+    private:
+    void reverse(vector<int>&arr,int s,int e)
+    {
+        while(s<=e)
+        {
+            swap(arr[s],arr[e]);
+            s++;
+            e--;
+        }
+
+    }
+    
 public:
     void nextPermutation(vector<int>& nums) {
-        // ios_base::sync_with_stdio(0);
-        // cin.tie(0);
-        // cout.tie(0);
         int n=nums.size();
-        int index=-1;
+        int ind=-1;
         for(int i=n-2;i>=0;i--)
         {
             if(nums[i]<nums[i+1])
             {
-                index=i;
+                ind=i;
                 break;
             }
         }
-        if(index==-1) {
-            reverse(nums.begin(),nums.end());
-            return;}
-        for(int i=n-1;i>=index;i--)
+        if(ind==-1){
+            reverse(nums,0,n-1);
+            return;
+        }
+        for(int i=n-1;i>=ind;i--)
         {
-            if(nums[i]>nums[index])
+            if(nums[i]>nums[ind])
             {
-                swap(nums[i],nums[index]);
+                swap(nums[i],nums[ind]);
                 break;
             }
         }
-        reverse(nums.begin()+(index+1),nums.end());
+        reverse(nums,ind+1,n-1);
     }
 };
