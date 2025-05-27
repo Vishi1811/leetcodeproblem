0bd99cff4c790fdee7b21996dac3edd4328d5645
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2025-05-27 14:22:59 +0530

    Time: 0 ms (100%), Space: 12.2 MB (95.47%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,43 +1,38 @@
 class Solution {
-private:
-    void Swap(vector<int>& nums1, vector<int>& nums2, int ind1, int ind2) {
-        if (nums1[ind1] > nums2[ind2]) {
-            swap(nums1[ind1], nums2[ind2]);
+    private: void swapIfGreater(vector<int>&nums1, int ind1, vector<int>&nums2, int ind2)
+    {
+        if(nums1[ind1]>nums2[ind2])
+        {
+            swap(nums1[ind1],nums2[ind2]);
         }
     }
-
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int l = m + n;  // Total length of the two arrays
-        int gap = (l + 1) / 2;  // Initialize the gap
-        
-        while (gap > 0) {
-            int left = 0;
-            int right = left + gap;
-            
-            while (right < l) {
-                // First array nums1 indices [0, m-1]
+        int len=m+n;
+        int gap=(len/2)+(len%2);
+        while(gap>0)
+        {
+            int left=0;
+            int right=left+gap;
+            while (right < len) {
+                // nums1 and nums1
                 if (left < m && right < m) {
-                    Swap(nums1, nums1, left, right);  // Swap within nums1
+                    swapIfGreater(nums1, left, nums1, right);
                 }
-                // Swap between nums1 and nums2
+                // nums1 and nums2
                 else if (left < m && right >= m) {
-                    Swap(nums1, nums2, left, right - m);  // Swap between nums1 and nums2
+                    swapIfGreater(nums1, left, nums2, right - m);
                 }
-                // Second array nums2 indices [m, m+n-1]
-                else if (left >= m) {
-                    Swap(nums2, nums2, left - m, right - m);  // Swap within nums2
+                // nums2 and nums2
+                else {
+                    swapIfGreater(nums2, left - m, nums2, right - m);
                 }
                 left++;
                 right++;
             }
-
-            // Reducing the gap for next pass
-            if (gap == 1) break;
-            gap = (gap + 1) / 2;
+            if(gap==1)break;
+            gap=(gap/2)+(gap%2);
         }
-        
-        // After sorting, merge nums2 into nums1
         for (int i = 0; i < n; i++) {
             nums1[m + i] = nums2[i];
         }
